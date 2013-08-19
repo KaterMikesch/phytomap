2ca413c262738bd47f1263f5dba0cf76f2ceb762
Axel Katerbau <axel@objectpark.org>
2013-08-19 15:13:35 +0200

    - global var experiment

--- src/cljs/phytomap/test.cljs
+++ src/cljs/phytomap/test.cljs
@@ -4,17 +4,21 @@
 (defn log [& more]
   (.log js/console (apply str more)))
 
-(defn nodes-callback [reply]
-  (let [nodes (js->clj (.getResponseJson (.-target reply)))]
-    (log nodes)))
+(defn make-nodes-map [raw-nodes-list]
+  )
+
+(def *raw-nodes* nil)
 
 (.send goog.net.XhrIo "http://localhost:3000/nodes.json" 
   (fn [result] 
     (let [nodes (js->clj (.getResponseJson (.-target result)))]
-    (.send goog.net.XhrIo "http://localhost:3000/stats.json"
-      #(let [stats (js->clj (.getResponseJson (.-target %)))]
-         (log "nodes: " nodes "\n\n\nstats: " stats)
-         (log "\n\nfirst node: " (first nodes)))))))
+      (set! *raw-nodes* nodes)
+      (log "\n HURZ: " *raw-nodes* "\n")
+      (.send goog.net.XhrIo "http://localhost:3000/stats.json"
+        #(let [stats (js->clj (.getResponseJson (.-target %)))]
+           ;(log "nodes: " nodes "\n\n\nstats: " stats)
+           ;(log "\n\nfirst node: " (first nodes))
+           )))))
 
 
 ;(log "toll " 34 " Dirky" (format "%s" "bla"))
